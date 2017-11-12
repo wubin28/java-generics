10a8f07a8f2b7318b701c2767840d7289710fc02
Ben Wu <wubin28@gmail.com>
2017-11-12 21:45:30 +0800

    add test you_can_read_from_a_list_of_unbounded_type_but_not_write_to_it

--- src/test/java/com/wubinben/katas/GenericsTest.java
+++ src/test/java/com/wubinben/katas/GenericsTest.java
@@ -2,7 +2,9 @@ package com.wubinben.katas;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GenericsTest {
@@ -14,4 +16,25 @@ public class GenericsTest {
         // List<Object> objects = strings;
         // objects = strings;
     }
+
+    @Test
+    public void you_can_read_from_a_list_of_unbounded_type_but_not_write_to_it() {
+        List<Integer> stuff = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
+        stuff.size();
+        stuff.forEach((Object o) -> System.out.println(o));
+
+        stuff.add(4);
+
+        stuff.forEach((Object o) -> System.out.println(o));
+
+        List<?> anotherStuff = new ArrayList<>(Arrays.asList("Hello", LocalDate.now(), 3));
+        anotherStuff.size();
+        anotherStuff.forEach((Object o) -> System.out.println(o));
+
+        // add(capture<?>) in List cannot be applied to (int)
+        // anotherStuff.add(4);
+
+        anotherStuff.forEach((Object o) -> System.out.println(o));
+    }
+
 }
